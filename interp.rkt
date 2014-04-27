315b826002dffeb136ec7a55938ae589e0b6aa76
Semih <Semyonic@users.noreply.github.com>
2014-04-27 23:34:35 +0300

    Update interp.rkt
    Updated to latest form.

--- interp.rkt
+++ interp.rkt
@@ -7,7 +7,7 @@
 ;S -> (fundef <ident> S)
 ;S -> (S S)
 ;
-;;Data Definition:
+;;Data Definition : 
 ;
 ;A sentence is either;  number,
 ;	                identifier,
@@ -15,73 +15,106 @@
 ;	                fundef,
 ;	                funapp
 (define-type sentence 
-  [sentence_num (n:number)]
-  [sentence_ident (id:symbol)]
-  [sentence_addition (lhs:sentence rhs:sentence)]
-  [sentence_funDef (param:symbol body:sentence)]
-  [sentence_funApp (applier:sentence applied:sentence)])
+  [sentence-num (n : number)]
+  [sentence-ident (id : symbol)]
+  [sentence-addition (lhs : sentence) (rhs : sentence)]
+  [sentence-fundef (param : symbol) (body : sentence)]
+  [sentence-funapp (applier : sentence) (applied : sentence)])
+
+
+
 
 ;An environment is either; empty,
-;	                   identifier,
-;	                   value,
-;	                   Env
-(define-type Env
-  [env_empty ()]
-  [env_ident (id:symbol) (val:value) (env:Env)])
+; OR
+;	                   identifier, value, env
+(define-type env
+  [env-empty ]
+  [env-ident (id : symbol) (val : value) (next : env)])
 
 ;A value is either; number,
 ;                   function value
 (define-type value
-  [value_number (n:number)]
-  [value_funcVal (env:Env) (body:Sentence) (fp:symbol)])
+  [value-number (n : number)]
+  [value-func-val  (fp : symbol) (body : sentence) (e : env) ]
+  [value-error (reason : symbol)])
 
-;; Contract: interpreter
+
+;; parser s-expression -> sentence
+;;
+;; examples
+;;
+;; '5 -> (sentence-num 5)
+;; other examples
+;; template
+(define (parse [s : s-expression])
+  (cond
+    ((s-exp-number? s) (sentence-num (s-exp->number s)))
+    ((s-exp-symbol? s) (sentence-ident (s-exp->symbol s)))
+    ((and (s-exp-list? s) (symbol=? (s-exp->symbol (first (s-exp->list s))) '+)
+          (= (length (s-exp->list s)) 3))
+     (sentence-addition (parse (second (s-exp->list s))) (parse (third (s-exp->list s)))))
+    ((and (s-exp-list? s) (symbol=? (s-exp->symbol (first (s-exp->list s))) 'fundef)
+          (s-exp-symbol? (second (s-exp->list s))) (= (length (s-exp->list s)) 3))
+     (sentence-fundef (s-exp->symbol (second (s-exp->list s))) (parse (third (s-exp->list s)))))
+    ((and (s-exp-list? s) (= (length (s-exp->list s)) 2))
+     (sentence-funapp (parse (first (s-exp->list s))) (parse (second (s-exp->list s)))))
+    (else (error 'parse (s-exp->string s)))))
+    
+    
+    
+     
+
+
+
+;; Contract :  interpreter
 ;Sentence, Env -> value
 
-;;Purpose: 
+;;Purpose :  
 ;Evaluate the sentence in the given environment to produce a value.
 
-;;Examples:
+;;Examples : 
 ; 5 (emty Env) -> 5 (value-number 5)
 ; 5 (x = 5) -> 5 (value-number 5)
 ; x (x = 2) -> 2 (value-number 2)
 ; (+ x y) (x=4 y=3) -> 7 (value-number 7)
 ; (fundef f S) (empty Env) -> (Env, Sentence, Symbol)
 ; (f x) (x=3) -> ERROR
-; (f x) (x=3, f=(funDef n (+ n 3)) -> 6
+; (f x) (x=3, f=(fundef n (+ n 3)) -> 6
 ; ((fundef x (+ x 3)) 3) (empty Env) -> 6
+; ((fundef x (+ x 5)) 5) (empty Env) -> 6
 
 ;;Template
-; (define (interp s:sentence env:Env)
+; (define (interp s : sentence env : Env)
 ;   (type-case s
-;     [sentence_num (n) ... ]
-;     [sentence_ident (id) ... ]
-;     [sentence_addition (lhs rhs) ... ]
-;     [sentence_funDef (param body) ... ]
-;     [sentence_funApp (applier applied) ... ]))
+;     [sentence-num (n) ... ]
+;     [sentence-ident (id) ... ]
+;     [sentence-addition (lhs rhs) ... ]
+;     [sentence-fundef (param body) ... ]
+;     [sentence-funapp (applier applied) ... ]))
 
 
-(define (interp s:sentence env:Env)
-  (type-case s
-    [sentence_num (n) (value-number (n))]
-    [sentence_ident (id) (look-up id env)]
-    [sentence_addition (lhs rhs) (value-number (+ (value-number-n (interp lhs env))
-                                                  (value-number-n (interp rhs env))))]
-    [sentence_funDef (param body) (value-fv env body param)]
-    [sentence_funApp (applier applied) (apply (interp applier env)
-                                              (interp applied env)
-                                              env)]))
+(define (interp [s : sentence] [e : env])
+  (type-case sentence s
+    [sentence-num (n) (value-number n)]
+    [sentence-ident (id) (look-up id e)]
+    [sentence-addition (lhs rhs) (value-number (+ (value-number-n (interp lhs e))
+                                                  (value-number-n (interp rhs e))))]
+    [sentence-fundef (param body) (value-func-val param body e)]
+    [sentence-funapp (applier applied) (apply (interp applier e)
+                                              (interp applied e)
+                                              e)]))
 
-;;;helper functions: 
+;;;helper functions :  
 ;; look-up
-(define (look-up id symbol env:Env)
-  (type-case env
-    [env_empty ERROR]
-    [env_full ...]))
+(define (look-up [id : symbol] [e : env])
+  (type-case env e
+    [env-empty () (value-error 'undefined-symbol)]
+    [env-ident (env-id env-val next-env)
+               (if (symbol=? id env-id) env-val (look-up id next-env))]))
 
-;; apply:     
-;value value env -> value
-(define (apply applier:value applied:value e:Env)
-  (type-case applier
-    [value-number error]
-    [value_fv (fv_env fv_body fv_fp) (interp fv_body (Env fv_fp applied fv_env))]))
+;; apply :      value value env -> value
+(define (apply [applier : value] [applied : value] [e : env])
+  (type-case value applier
+    [value-number (n) (value-error 'applied-but-not-a-function-value)]
+    [value-error (err) (value-error 'applied-but-not-a-function-value)]
+    [value-func-val (fv-fp fv-body fv-env) (interp fv-body (env-ident fv-fp applied fv-env))]))
